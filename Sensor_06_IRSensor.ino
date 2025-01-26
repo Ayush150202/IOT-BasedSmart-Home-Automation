ffc85226918b590084140f7aecc02c4028763306
Ayush150202 <ayushrajpoot633@gmail.com>
2025-01-26 11:43:57 +0530

    Create Sensor_06_IRSensor.ino

---
+++ Sensor_06_IRSensor.ino
@@ -0,0 +1,18 @@
+void setup() {
+pinMode(A3,INPUT);
+pinMode(44,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+Serial.println(analogRead(A3));
+delay(1000);
+if(analogRead(A3) < 250)
+{
+  digitalWrite(44,HIGH);
+}
+else
+{
+  digitalWrite(44,LOW);
+}
+}
