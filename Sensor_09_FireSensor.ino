4cf186ea580eb8ff988e52ee9f00086500fe9636
Ayush150202 <ayushrajpoot633@gmail.com>
2025-01-26 11:45:22 +0530

    Create Sensor_09_FireSensor.ino

---
+++ Sensor_09_FireSensor.ino
@@ -0,0 +1,31 @@
+#include<SoftwareSerial.h>
+int sensorPin = A1;
+int sensorValue = 0;
+int led = 51;
+int buzzer = 50;
+
+void setup() {
+pinMode(led, OUTPUT);
+pinMode(buzzer,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{
+Serial.println("Welcome to Flame Sensor Tutorial");
+sensorValue = analogRead(sensorPin);
+Serial.println(sensorValue);
+if (sensorValue < 100)
+{
+Serial.println("Fire Detected");
+Serial.println("LED ON");
+digitalWrite(led,HIGH);
+Serial.println("BUZZER ON");
+digitalWrite(buzzer,HIGH);
+delay(1000);
+
+}
+digitalWrite(led,LOW);
+digitalWrite(buzzer,LOW);
+delay(sensorValue);
+}
